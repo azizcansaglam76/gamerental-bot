6d85d2d1d33a98fb3f1dde85d305396c09397aaa
azizcansaglam76 <azizcansaglam76@gmail.com>
2026-03-12 14:45:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const admin = require('firebase-admin');
 const axios = require('axios');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 
 const CONFIG = {
